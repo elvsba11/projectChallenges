100f52ea76023c73a7d65ec208fa030751c808da
elvsba11 <107898470+elvsba11@users.noreply.github.com>
2026-06-04 21:05:22 +0800

    New Challenge

---
+++ JS-Warm-Up-Methods-ES6/05-store-analytics.js
@@ -0,0 +1,68 @@
+let sales = [
+  {
+    orderId: 5001,
+    customer: "Juan",
+    items: ["T-Shirt", "Caps"],
+    total: 1500,
+    status: "Delivered",
+  },
+  {
+    orderId: 5002,
+    customer: "Maria",
+    items: ["Dress"],
+    total: 2500,
+    status: "Pending",
+  },
+  {
+    orderId: 5003,
+    customer: "Pedro",
+    items: ["Shoes", "Socks"],
+    total: 4500,
+    status: "Delivered",
+  },
+  {
+    orderId: 5004,
+    customer: "Kiko",
+    items: ["Cap"],
+    total: 600,
+    status: "Cancelled",
+  },
+  {
+    orderId: 5005,
+    customer: "Lisa",
+    items: ["Jackets"],
+    total: 3200,
+    status: "Delivered",
+  },
+];
+
+//1. filter + Destructuring + forEach
+console.log("\n=======Order Status=======\n");
+const orderStatus = sales.filter(({ status }) => status === "Delivered");
+// Log each status = delivered
+orderStatus.forEach(({ customer, total }) => {
+  console.log(`Thank you ${customer} for buying, worth of ₱${total}!`);
+});
+
+//2. map + Rest/Spread Operator
+console.log("\n=======Shipping Fee=======\n");
+const shippingFee = sales.map(({ total, ...rest }) => {
+  return {
+    total,
+    ...rest,
+    shippingFee: total >= 3000 ? 0 : 150,
+  };
+});
+
+console.log(shippingFee);
+
+//3. reduce + Destructuring
+console.log("\n=======Total Revenue=======\n");
+const orderDelivered = sales.filter(({ status }) => status === "Delivered");
+
+// function totalRevenue() {
+//   return orderDelivered.reduce((sum, { total }) => sum + total, 0);
+// }
+const total = orderDelivered.reduce((sum, { total }) => sum + total, 0);
+
+console.log(total);
